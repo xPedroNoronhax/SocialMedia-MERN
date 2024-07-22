5506bc5e288acad131691a10dd8c4d2c415483b4
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-22 15:28:05 -0300

    add header a userpage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { Container } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Container maxW="620px">
+      <Header />
       <Routes>
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />

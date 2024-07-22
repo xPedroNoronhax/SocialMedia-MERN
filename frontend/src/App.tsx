440a0eff4b20c84578f1984eecd7f03ef5255f4e
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-22 14:25:04 -0300

    Add frontend folder as regular folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+
+const App = () => {
+  return (
+    <Container maxW="620px">
+      <Routes>
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pid" element={<PostPage />} />
+      </Routes>
+    </Container>
+  );
+};
+
+export default App;

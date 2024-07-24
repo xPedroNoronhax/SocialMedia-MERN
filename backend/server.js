0dfc4f23929fe6523de992f984c42b6a4de8e83e
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-24 15:26:17 -0300

    add all feats until now

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from './db/connectDB.js'
+import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js"; // Importe suas rotas
 
 dotenv.config();
 
 connectDB();
+
 const app = express();
+
+app.use(express.json()); // Middleware para parsing de JSON
+
+app.use("/api/users", userRoutes); // Defina a rota base para suas rotas de usuários
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(3000, () => console.log(`SERVER STARTED AT PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));

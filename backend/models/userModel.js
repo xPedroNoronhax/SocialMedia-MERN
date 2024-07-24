5d60680aff644fd94bb48ac1ff0a5e30b3685262
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-24 11:50:36 -0300

    add postModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -43,6 +43,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;

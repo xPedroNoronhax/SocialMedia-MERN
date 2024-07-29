68775d9137141253a65043ca5543fa8ab8c4b35c
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-29 16:42:15 -0300

    add postController,createPost

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createPost, getPost } from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+const router = express.Router();
+router.get("/:id", getPost);
+router.post("/create", protectRoute, createPost);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser"; // Importe cookie-parser
 import userRoutes from "./routes/userRoutes.js"; // Importe suas rotas
+import postRoutes from "./routes/postRoutes.js";
 
 dotenv.config();
 
@@ -11,9 +12,11 @@ connectDB();
 const app = express();
 
 app.use(express.json()); // Middleware para parsing de JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Use cookie-parser middleware
 
 app.use("/api/users", userRoutes); // Defina a rota base para suas rotas de usuários
+app.use("/api/post", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 

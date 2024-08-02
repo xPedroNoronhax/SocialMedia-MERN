6aa02f016c1ed3d89d1678f945cafbb422d4d3d7
Pedro Noronha <pedro_noronha@outlook.com>
2024-08-02 13:28:05 -0300

    add feed post in home funtionality

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -145,7 +145,7 @@ const getFeedPost = async (req, res) => {
     const feedPost = await Post.find({ postedBy: { $in: following } }).sort({
       createdAt: -1,
     });
-    res.status(200).json({ feedPost });
+    res.status(200).json( feedPost );
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log(error);
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,73 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-import { Link } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
+
+type User = {
+  _id: string;
+  username: string;
+  profilePic?: string;
+};
+
+type Reply = {
+  userId: User;
+  text: string;
+  userProfilePic?: string;
+  username?: string;
+};
+
+type PostData = {
+  _id: string;
+  postedBy: string; // Atualizado para string (ID) em vez de User
+  text: string;
+  img?: string;
+  likes: User[];
+  replies: Reply[];
+  createdAt: string;
+};
 
 const HomePage = () => {
+  const [posts, setPosts] = useState<PostData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        console.log("Feed Posts:", data); // Verifique os dados recebidos
+        if (data.error) {
+          showToast("Error", "Error in getting the feed posts", "error");
+          return;
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", "Error in getFeedPosts", "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
   return (
-    <Link to={"/markzuckerberg"}>
-      <Flex w={"full"} justifyContent={"center"}>
-        <Button mx={"auto"}>Visit Profile Page</Button>
-      </Flex>
-    </Link>
+    <>
+      {!loading && posts.length === 0 && (
+        <h1>Follow some users to see the feed</h1>
+      )}
+
+      {loading && (
+        <Flex justify={"center"}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </>
   );
 };
 
